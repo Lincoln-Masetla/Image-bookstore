43ba9b80260183cf355f9be717eb24672b21acdf
Linco <Linco.Masetla@eoh.com>
2022-02-08 10:57:13 +0200

    add

---
+++ Clean-Architecture-ASP.NET-Core-develop/src/CleanArch.Application/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace CleanArch.Application.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
